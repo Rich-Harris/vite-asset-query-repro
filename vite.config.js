14de747146d5c0124d468e5dc0a77bd2d807bb00
Rich Harris <hello@rich-harris.dev>
2023-02-01 17:01:21 -0500

    add manifest

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		assetsInlineLimit: 0,
+		manifest: true,
 		rollupOptions: {
 			input: 'src/index.js',
 			output: {
